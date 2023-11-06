d9d71cf73bb05c4aba6530a821890f0ee56831d8
patoangelaa <148946000+patoangelaa@users.noreply.github.com>
2023-11-06 13:44:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+
+    // displayMessage('9.B for president!');
+    const guess = Number (document.querySelector('.guess').value);
+    if (!guess) {
+    displayMessage('Nem szám!');
+}
+else if (guess === secretNumber) {
+    displayMessage('Eltaláltad!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+if (score > highscore) {
+    highscore = score;
+    document.querySelector('.highscore').textContent = highscore;
+}
+}
+else if (guess !== secretNumber) {
+    if (score > 1) {
+        displayMessage(guess > secretNumber ? 'Túl sok!' : 'Túl kevés!');
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+    else {
+        displayMessage('Vesztettél!');
+        document.querySelector('.score').textContent = 0;
+    }
+}
+});
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    displayMessage('Találgass...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+
+});
